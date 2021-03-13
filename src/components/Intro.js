9c5754a1901581e789a584ff73e11c2d8b33c574
robdcampbell <rdeancampbell@gmail.com>
2021-03-13 16:08:37 -0500

    Add Daily goal content to intro section - wire it up next

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -4,29 +4,34 @@ const Intro = () => {
   return (
     <section className="intro__section">
       <div className="titles__intro">
-        <div className="heading__left">
-          <h3>Segment -</h3>
-          <h3>breakdown:</h3>
+        <div className="intro__left">
+          <div>
+            <h3>Segment -</h3>
+            <h3>breakdown:</h3>
+          </div>
         </div>
         <div className="heading__right">
           <h2>__ Pomodoro Style.</h2>
         </div>
       </div>
 
-      {/* <div className="headings__bottom"> */}
-      <div className="intro__content">
-        <p className="intro__text">
-          The Pomodoro Technique helps you resist all of those
-          self-interruptions and re-train your brain to focus. Each "pomodoro"
-          is dedicated to one task and each break is a chance to reset and bring
-          your attention back to what you should be working on.
-        </p>
-        <p className="intro__text">
-          Our "normal" day can snowball. <strong>Agenda</strong> breaks 8 hrs
-          into segments (with breaks), and keeps you from going down a rabbit
-          hole.
-        </p>
-        {/* </div> */}
+      <div className="intro__bottom">
+        <h3 className="add__daily">
+          <span>+</span> Add daily goals.
+        </h3>
+        <div className="intro__content">
+          <p className="intro__text">
+            The Pomodoro Technique helps you resist all of those
+            self-interruptions and re-train your brain to focus. Each "pomodoro"
+            is dedicated to one task and each break is a chance to reset and
+            bring your attention back to what you should be working on.
+          </p>
+          <p className="intro__text">
+            Our "normal" day can snowball. <strong>Agenda</strong> breaks 8 hrs
+            into segments (with breaks), and keeps you from going down a rabbit
+            hole.
+          </p>
+        </div>
       </div>
     </section>
   );
